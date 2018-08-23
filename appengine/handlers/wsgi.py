517090658014def37a459cd2bea05999b9b045ef
infojunkie <karim.ratib@gmail.com>
2018-08-22 17:12:44 -0700

    Hard-redirect Storyful to Montage URL #18

--- appengine/handlers/wsgi.py
+++ appengine/handlers/wsgi.py
@@ -1,10 +1,33 @@
 import fix_paths
 from django.core.wsgi import get_wsgi_application
 from greenday_core.utils import get_settings_name
+from urllib import quote
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", get_settings_name())
 
 from .middleware import error_logging_middleware
 
-application = error_logging_middleware(get_wsgi_application())
+_application = error_logging_middleware(get_wsgi_application())
 
+def reconstruct(environ, server_name):
+  # https://www.python.org/dev/peps/pep-0333/#url-reconstruction
+  url = environ['wsgi.url_scheme']+'://'
+
+  url += server_name
+
+  url += quote(environ.get('SCRIPT_NAME', ''))
+  url += quote(environ.get('PATH_INFO', ''))
+  if environ.get('QUERY_STRING'):
+    url += '?' + environ['QUERY_STRING']
+
+  return url
+
+def application(environ, start_response):
+  # hard-redirect 'montage.storyful.com' to 'montage.meedan.com'
+  if environ['SERVER_NAME'] == 'montage.storyful.com':
+    start_response('301 Redirect', [
+      ('Location', reconstruct(environ, 'montage.meedan.com')),
+    ])
+    return []
+
+  return _application(environ, start_response)
